62c2ea56d1a768f0553e1fa05e397d0fa404716b
Ethem Serkan Angün <serkan.angun@ecospend.com>
2022-11-12 14:16:29 +0300

    Nuget packages are installed & Goal Model is created

---
+++ ToDoAPI/Models/Domain/Goal.cs
@@ -0,0 +1,11 @@
+﻿namespace ToDoAPI.Models.Domain
+{
+    public class Goal
+    {
+        public int Id { get; set; }
+        public string? Topic { get; set; }
+        public string? Description { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public int? Period { get; set; }
+    }
+}

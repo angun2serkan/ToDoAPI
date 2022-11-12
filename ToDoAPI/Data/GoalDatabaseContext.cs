eb81226bc0894b70d5dd7967be23ee35d2432aa8
Ethem Serkan Angün <serkan.angun@ecospend.com>
2022-11-12 14:45:13 +0300

    create DbContext, Connection String, injected DbContext class & add migrations

---
+++ ToDoAPI/Data/GoalDatabaseContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ToDoAPI.Models.Domain;
+
+namespace ToDoAPI.Data
+{
+    public class GoalDatabaseContext : DbContext
+    {
+        public GoalDatabaseContext(DbContextOptions<GoalDatabaseContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Goal> Goals { get; set; }
+    }
+}
---
+++ ToDoAPI/Migrations/20221112114007_InitialMigration.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ToDoAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Goals",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Topic = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    CreatedDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Period = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Goals", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Goals");
+        }
+    }
+}

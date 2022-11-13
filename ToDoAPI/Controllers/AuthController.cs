8a07b4a2d70e57c4880448a5a094b8c600c0b636
Ethem Serkan Angün <serkan.angun@ecospend.com>
2022-11-13 18:33:25 +0300

    Auth controller is created

--- ToDoAPI/Controllers/AuthController.cs
+++ ToDoAPI/Controllers/AuthController.cs
@@ -1,12 +1,36 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using ToDoAPI.Repositories;
 
 namespace ToDoAPI.Controllers
 {
+    [ApiController]
+    [Route("[controller]")]
     public class AuthController : Controller
     {
-        public IActionResult Index()
+        private readonly IUserRepository _userRepository;
+        public AuthController(IUserRepository userRepository)
         {
-            return View();
+            _userRepository=userRepository;
+        }
+        [HttpPost]
+        [Route("login")]
+        public async Task<IActionResult> LoginAsync(Models.DTO.LoginRequest loginRequest)
+        {
+            //validate the incoming request
+
+            //check if user is authenticated
+            // check username and password
+
+            var isAuthenticated = await _userRepository.AuthenticateAsync(
+                loginRequest.Username, loginRequest.Password);
+
+            if (isAuthenticated)
+            {
+
+                // generate a jwt token
+            }
+
+            return BadRequest("Username or password is incorrect.");
         }
     }
 }
